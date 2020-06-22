f18e5e5742f19baba4e4685ae0fd29ca04cb2dee
wzy <wzypku86@gmail.com>
2020-06-22 17:16:50 -0400

    change file path

---
+++ client.py
@@ -0,0 +1,117 @@
+from socket import *
+import sys
+import pickle
+
+INPUT_NUM = 4
+
+def negotiaitonTCP(server_address, n_port, req_code):
+    '''Negotiation Phase communication over TCP
+
+    Parameters:
+    server_address -- the server address
+    n_port -- negotiation port number of the server
+    req_code -- request code for negotiation
+
+    Returns:
+    r_port -- random port number of the transaction
+    '''
+    try:
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.settimeout(10.0)
+        client_socket.connect((server_address, n_port))
+
+        client_socket.send(str(req_code).encode())
+        r_port = int(client_socket.recv(1024).decode())
+        client_socket.close()
+    except ConnectionRefusedError:
+        sys.stderr.write("Error server_unavailable\n")
+        file = open("client.txt", "a+")
+        try:
+            file.write("Error server_unavailable\n")
+        finally:
+            file.close()
+        sys.exit(1)
+    
+    return r_port
+
+def transactionUDP(server_address, r_port, msg):
+    '''Transaction Phase communication over UDP
+
+    Parameters:
+    server_address -- the server address
+    r_port -- random port number of the transaction
+    msg -- the message to be sent
+
+    Returns:
+    None 
+    '''
+    try:
+        client_socket = socket(AF_INET, SOCK_DGRAM)
+        client_socket.settimeout(10.0)
+        client_socket.sendto("GET".encode(),(server_address, r_port))
+        messages, _ = client_socket.recvfrom(2048)
+        message_list = eval(messages.decode())
+        print('\n'.join(message_list))
+        log_port(r_port, message_list)
+        client_socket.sendto(msg.encode(), (server_address, r_port))
+    except ConnectionRefusedError:
+        sys.stderr.write("Error server_unavailable\n")
+        file = open("client.txt", "a+")
+        try:
+            file.write("Error server_unavailable\n")
+        finally:
+            file.close()
+        sys.exit(1)
+
+    input("Press Enter to continue...")
+    client_socket.close()
+
+def log_port(r_port, message_list):
+    '''Write the line <r_port> to that file with the messages in order
+
+    Parameters:
+    r_port -- random port number of the transaction
+    msg -- the message to be sent
+
+    Returns:
+    None
+    '''
+
+    existed = False
+
+    file = open("client.txt", "a+")
+
+    try:
+        next_line = "r_port: " + str(r_port) + "\n"
+        file.write(next_line)
+
+        for msg in message_list:
+            file.write(str(msg) + "\n")
+    finally:
+        file.close()
+
+def main():
+    if len(sys.argv) - 1 == INPUT_NUM:
+        server_address = sys.argv[1]
+        n_port = int(sys.argv[2])
+        req_code = int(sys.argv[3])
+        msg = str(sys.argv[4])
+    else:
+        sys.stderr.write("Invalid Arguments: should input " + INPUT_NUM + " parameters\n")
+        sys.exit(1)
+    
+    r_port = negotiaitonTCP(server_address, n_port, req_code)
+
+    if r_port == 0:
+        sys.stderr.write("Invalid req_code\n")
+        file = open("client.txt", "a+")
+        try:
+            file.write("Invalid req_code\n")
+        finally:
+            file.close()
+        sys.exit(2)
+    
+    transactionUDP(server_address, r_port, msg)
+
+if __name__ == "__main__":
+    main()
